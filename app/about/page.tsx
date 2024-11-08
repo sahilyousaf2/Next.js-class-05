5769e3d573d841a49b75a7d1c9b412d2812fd1f9
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-11-08 12:23:55 +0500

    Add files via upload
    final

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import Main from "../components/Main";
+
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
